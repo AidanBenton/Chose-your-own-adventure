d77b8915415f9c289345727878a867f98cb425b5
AidanBenton <aidanbenton2005@gmail.com>
2021-10-26 21:14:59 -0400

    worked on code

--- Chose your own adventure/Form1.cs
+++ Chose your own adventure/Form1.cs
@@ -16,6 +16,8 @@ namespace Chose_your_own_adventure
         // The page of the story the user is currently at
         int page = 1;
         int snake = 0;
+        int luggage = 0;
+        int randomNumber = 0;
         public Form1()
         {
             InitializeComponent();
@@ -25,8 +27,11 @@ namespace Chose_your_own_adventure
         {
             if (page == 1)
             {
+                //Player gets to chose item of choice
+                luggage = 1;
                 page = 2; 
             }
+                //Player choses there path
             else if (page == 2) {page = 3;}
             else if (page == 3) {page = 1;}
             else if (page == 4) {page = 5;}
@@ -42,35 +47,118 @@ namespace Chose_your_own_adventure
             else if (page == 9) {page = 1;}
             else if (page == 10)
             {
-                if (luggage = 2 || snake = 1)
+                //gameplay element including food
+                if (luggage == 2 || snake == 1)
                 {
-                    page = 13;
+                    page = 11;
                 }
                 else
                 {
-                    page = 24; 
+                    page = 23; 
                 }
             }
-            else if (page == 11) { }
-            else if (page == 12) { }
-            else if (page == 13) { }
-            else if (page == 14) { }
-            else if (page == 15) { }
-            else if (page == 16) { }
-            else if (page == 17) { }
-            else if (page == 18) { }
-            else if (page == 19) { }
-            else if (page == 20) { }
-            else if (page == 21) { }
-            else if (page == 22) { }
-            else if (page == 23) { }
-            else if (page == 24) { }
-            else if (page == 99) { }
+            else if (page == 11) {page = 1;}
+            else if (page == 12) {page = 13;}
+            else if (page == 13) 
+            {
+                //decididng if the chimps attack
+                if (luggage == 1)
+                {
+                    page = 15;
+                }
+                else
+                {
+                    page = 16; 
+                }
+
+            }
+            //Player chos
+            else if (page == 14) {page = 1;}
+            else if (page == 15) {page = 1;}
+            else if (page == 16) {page = 1;}
+            else if (page == 17) {page = 13;}
+            else if (page == 18) {page = 1;}
+            else if (page == 19) 
+            { 
+                if (luggage == 1)
+                {
+                    page = 22; 
+                }
+                else
+                {
+                    page = 21; 
+                }
+            }
+            else if (page == 20) {page = 1;}
+            else if (page == 21) {page = 1;}
+            else if (page == 22) {page = 1;}
+            else if (page == 23) {page = 1;}
         }
 
         private void optionButtonTwo_Click(object sender, EventArgs e)
         {
-
+            if (page == 1)
+            {
+                //Player gets to chose item of choice
+                luggage = 2;
+                page = 2;
+            }
+            else if (page == 2) { page = 4; }
+            else if (page == 3) { page = 99; }
+            else if (page == 4) 
+            {
+                if (randomNumber > 3)
+                {
+                    if (luggage == 1)
+                    {
+                        page = 17;
+                    }
+                    else
+                    {
+                        page = 10;
+                    }
+                }
+                else if (randomNumber <= 3)
+                {
+                    page = 99;
+                }
+                
+            }
+            else if (page == 5) 
+            {
+                if (luggage == 3)
+                {
+                    page = 8;
+                }
+                else
+                {
+                    page = 7;
+                }
+            }
+            else if (page == 6) {page = 99;}
+            else if (page == 7) {page = 99;}
+            else if (page == 8)
+            {
+                page = 10;
+            }
+            else if (page == 9) {page = 99;}
+            else if (page == 10)
+            {
+                page = 11;
+            }
+            else if (page == 11) {page = 99;}
+            else if (page == 12) {page = 19;}
+            else if (page == 13) {page = 14;}
+            else if (page == 14) {page = 99;}
+            else if (page == 15) {page = 99;}
+            else if (page == 16) {page = 99;}
+            else if (page == 17) {page = 18;}
+            else if (page == 18) {page = 99;}
+            else if (page == 19) {page = 20;}
+            else if (page == 20) {page = 99;}
+            else if (page == 21) {page = 99;}
+            else if (page == 22) {page = 99;}
+            else if (page == 23) {page = 99;}
         }
     }
 }
